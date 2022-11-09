ddd183f00dca306b40ac08e80df9835f4f90326a
palomo105 <116197323+palomo105@users.noreply.github.com>
2022-11-09 20:40:33 +0100

    Estructura de las clases

---
+++ src/main/java/telemarket/data/ProductoDB.java
@@ -0,0 +1,24 @@
+package telemarket.data;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author Miguel
+ */
+public class ProductoDB {
+    
+    //Busca en la base de datos un producto y lo devuelve
+    public static Producto getProducto(int nProducto){
+        return null;
+    }
+    
+    //Recoge de la base de datos todos los productos y los devuelve
+    public static ArrayList<Producto> getAllProductos(){
+        return null;
+    }
+    
+    //Recoge de la base de datos todos los productos de una categoria
+    public static ArrayList<Producto> getProductos(String categoria){
+        return null;
+    }
+}
